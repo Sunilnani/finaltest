d04c3678cb4baf5c71eb34940fa4b8cf0cf76f70
Sunilnani <42605686+Sunilnani@users.noreply.github.com>
2020-12-08 13:00:00 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _MyAppState extends State<MyApp> {
   List<String> text2=["Public","Bennee feat.Gus Dappertion","Trevor Daniel","Surface","Lewis Capaldi"];
   List<String> text3=["Make You Mine","Sapolny","Falling","Sunday Best","Someone You Liked"];
   List<String> text4=["3:30","3:40","3:30","3:40","3:30",];
+  List <Color> colors=[Colors.white,Colors.white,Colors.purpleAccent,Colors.white,Colors.white];
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +39,12 @@ class _MyAppState extends State<MyApp> {
                          text2: text2[index],
                          text3: text3[index],
                          text4: text4[index],
+                          colors: colors[index],
                         );
                       },
                     ),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(height: 5,),
                   CircleAvatar(
                     backgroundColor: Colors.red,
                     radius: 85,
@@ -56,7 +58,7 @@ class _MyAppState extends State<MyApp> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(height: 8,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -78,7 +80,17 @@ class _MyAppState extends State<MyApp> {
                           ],
 
                         ),
-                      )
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.shuffle,),
+                      Icon(Icons.arrow_back_outlined,size: 30,),
+                      Icon(Icons.play_circle_filled,color: Colors.red,size: 30,),
+                      Icon(Icons.arrow_forward,size: 30,),
+                      Icon(Icons.menu,size: 30,)
                     ],
                   )
                 ],
@@ -86,59 +98,75 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
+
       )
     );
   }
 }
 
-class Music extends StatelessWidget {
+class Music extends StatefulWidget {
   const Music({
     this.text1,
     this.text2,
     this.text3,
     this.text4,
+    this.colors,
     Key key,
   }) : super(key: key);
   final String text1;
   final String text2;
   final String text3;
   final String text4;
+  final Color colors;
+
+  @override
+  _MusicState createState() => _MusicState();
+}
+
+class _MusicState extends State<Music> {
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              child: Row(
-                children: [
-                  Text(text1,style: TextStyle(color: Colors.black87,fontWeight: FontWeight.w400),),
-                  SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(text2,style: TextStyle(color: Colors.black26,fontSize: 15,fontWeight: FontWeight.w400),),
-                      SizedBox(height: 10,),
-                      Text(text3,style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,fontSize: 16),)
-                    ],
-                  )
-                ],
+        Container(
+          height: 80,
+          decoration: BoxDecoration(
+            color:widget.colors,
+            borderRadius: BorderRadius.circular(30)
+          ),
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Text(widget.text1,style: TextStyle(color: Colors.black87,fontWeight: FontWeight.w400),),
+                    SizedBox(width: 10,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.text2,style: TextStyle(color: Colors.black26,fontSize: 15,fontWeight: FontWeight.w400),),
+                        SizedBox(height: 10,),
+                        Text(widget.text3,style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,fontSize: 16),)
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              child: Row(
-                children: [
-                  Text(text4,style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.w500),),
-                  SizedBox(width: 10,),
-                  Icon(Icons.favorite_border_outlined,color: Colors.black26,)
-                ],
-              ),
-            )
-          ],
+              Container(
+                child: Row(
+                  children: [
+                    Text(widget.text4,style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.w500),),
+                    SizedBox(width: 10,),
+                    Icon(Icons.favorite_border_outlined,color: Colors.black26,)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-        SizedBox(height: 20,)
+        SizedBox(height: 10,)
       ],
     );
   }
